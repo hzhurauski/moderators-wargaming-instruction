73d215a3950d359578ef877d4f0c1991b1140101
web-front-dev <herman.zhuravsky@gmail.com>
2021-12-29 21:58:48 +0300

    bread crumb update

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,5 +1,6 @@
 import { appTitle } from 'data'
 import { AppTitleType } from 'types/data/DataTypes'
+import { RoutesType } from 'types/routes/RoutesType'
 
 export const setTitleHelper = (pathname: string): string => {
   const defaultTitle = {
@@ -11,4 +12,97 @@ export const setTitleHelper = (pathname: string): string => {
     defaultTitle
 
   return title
+}
+
+export const setRoutes = (
+  url: string[],
+  routes: RoutesType[],
+  index: number
+): RoutesType[] => {
+  if (index === url.length) {
+    return routes
+  } else {
+    switch (url[index]) {
+      case '':
+      case '/home':
+        if (index !== 0) {
+          break
+        }
+        routes.push({
+          path: '/',
+          breadcrumbName: 'home',
+          children: [
+            {
+              path: '/network/search/vk',
+              breadcrumbName: 'Networks',
+            },
+          ],
+        })
+        break
+      case 'network':
+        routes.push({
+          path: '/',
+          breadcrumbName: 'network',
+          children: [
+            {
+              path: '/network/search/vk',
+              breadcrumbName: 'Search',
+            },
+            {
+              path: '/network/formalization/vk',
+              breadcrumbName: 'Formalization',
+            },
+          ],
+        })
+        break
+      case 'formalization':
+        routes.push({
+          path: '/network/formalization/vk',
+          breadcrumbName: 'formalization',
+          children: [
+            {
+              path: '/network/formalization/vk',
+              breadcrumbName: 'Vk',
+            },
+          ],
+        })
+        break
+      case 'search':
+        routes.push({
+          path: '/network/search/vk',
+          breadcrumbName: 'search',
+          children: [
+            {
+              path: '/network/search/vk',
+              breadcrumbName: 'Vk',
+            },
+            {
+              path: '/network/search/telegram',
+              breadcrumbName: 'Telegram',
+            },
+            {
+              path: '/network/search/discord',
+              breadcrumbName: 'Discord',
+            },
+            {
+              path: '/network/search/instagram',
+              breadcrumbName: 'Instagram',
+            },
+          ],
+        })
+        break
+      case 'vk':
+      case 'telegram':
+      case 'discord':
+      case 'instagram':
+        routes.push({
+          path: url[index],
+          breadcrumbName: url[index],
+        })
+        break
+    }
+  }
+
+  index += 1
+  return setRoutes(url, routes, index)
 }
